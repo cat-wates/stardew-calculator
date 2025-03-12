b4b4eaaca9c36314fdf77aedd97d9524e68ab85b
Cat Wates <Catherine.Wates@autotrader.co.uk>
2025-03-12 21:13:32 +0000

    Fixing player->farm join

--- src/main/java/uk/co/stardewcalculator/domain/Player.java
+++ src/main/java/uk/co/stardewcalculator/domain/Player.java
@@ -16,21 +16,24 @@ public class Player implements Serializable {
     private String username;
     private double farmingLevel;
     private int balance;
+
     @JsonProperty
     private boolean isTiller; //level 5
+
     @JsonProperty
     private boolean isAgriculturist; //level 10
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "farm_id")
     private Farm farm;
 
-    public Player(String username, double farmingLevel, int balance, boolean isTiller, boolean isAgriculturist) {
+    public Player(String username, double farmingLevel, int balance, boolean isTiller, boolean isAgriculturist, Farm farm) {
         this.username = username;
         this.farmingLevel = farmingLevel;
         this.balance = balance;
         this.isTiller = isTiller;
         this.isAgriculturist = isAgriculturist;
+        this.farm = farm;
     }
 
     public Player() {}
@@ -55,6 +58,10 @@ public class Player implements Serializable {
         return isAgriculturist;
     }
 
+    public Farm getFarm() {
+        return farm;
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
@@ -73,5 +80,9 @@ public class Player implements Serializable {
 
     public void setAgriculturist(boolean isAgriculturist) {
         this.isAgriculturist = isAgriculturist;
+    }
+
+    public void setFarm(Farm farm) {
+        this.farm = farm;
     }
 }
